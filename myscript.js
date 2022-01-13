c42bf741998204d6939b388bf95dad7a46172f5e
VincenzoVitello <92366183+VincenzoVitello@users.noreply.github.com>
2022-01-13 16:08:32 +0100

    stampati a schermo elementi iniettati con vue

---
+++ myscript.js
@@ -0,0 +1,7 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello World!',
+      img: 'https://www.google.ch/images/branding/googlelogo/2x/googlelogo_color_160x56dp.png'
+    }
+  })
